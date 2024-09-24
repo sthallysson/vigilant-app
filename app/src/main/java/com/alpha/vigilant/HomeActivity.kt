9e9f20303c403a275c79e87045eb44f5e2094924
sthallysson <sthallysson5@gmail.com>
2024-09-24 00:19:34 -0300

    feat: create HomeActivity and implement screen navigation

--- app/src/main/java/com/alpha/vigilant/HomeActivity.kt
+++ app/src/main/java/com/alpha/vigilant/HomeActivity.kt
@@ -1,32 +1,20 @@
 package com.alpha.vigilant
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        val btnLogin = findViewById<Button>(R.id.button_entrar)
-
-        btnLogin.setOnClickListener{
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
     }
 }
